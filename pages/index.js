006132d1a657041cc3070343c0f095c4443c1de2
LuigiHub97 <luigiscaglionejob2@outlook.com>
2026-03-05 14:55:52 +0000

    coloquei fotos

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,12 @@ export default function Home() {
 
       <div style={styles.photosContainer}>
         <img
-          src="/.jpeg"
+          src="/foto 1.jpeg"
           alt="nossa foto 1"
           style={styles.photo}
         />
         <img
-          src="/.jpeg"
+          src="/foto 2.jpeg"
           alt="nossa foto 2"
           style={styles.photo}
         />
