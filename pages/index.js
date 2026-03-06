2e836cf659ad61eca72d5bc13eea00e8d107db69
LuigiHub97 <luigiscaglionejob2@outlook.com>
2026-03-06 12:54:41 +0000

    adiciona scripts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function Home() {
-  return <h1>Crescer é um processo</h1>
+  return <h1>Crescer é um processo</h1>;
 }
 
-function teste(){
-  console.log ('teste');
+function teste() {
+  console.log("teste");
 }
 
+function teste2() {
+  console.log("identação errada");
+}
 export default Home;
