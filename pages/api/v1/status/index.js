62a71d95c031849f4fc729f93f914551dc77459c
Luigi Scaglione <luigiscaglionejob2@outlook.com>
2026-03-23 18:55:21 +0000

    add `database.js`and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({ chave: "valor" });
-}
+import database from "../../../../infra/database";
 
-export default status;
+export default async function status(req, res) {
+  try {
+    const result = await database.query("SELECT 1 as sum;");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.log("ERRO REAL:", err); // 👈 ISSO
+    res.status(500).json({ error: err.message });
+  }
+}
